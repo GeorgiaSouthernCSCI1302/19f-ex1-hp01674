751d50fcf1f67273a1d84fca1e93765ec13d8aba
hp01674 <hp01674@CEIT2553214X028.ad.georgiasouthern.edu>
2019-08-21 16:45:20 -0400

    Added a scanner asking user for their last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,10 +1,14 @@
 package assignment;
 
+import java.util.Scanner; 
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
 		System.out.println("Hello, my name is Holly.");
-		
+		System.out.print("What is your last name? "); 
+		String value = input.nextLine(); 
+	
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
